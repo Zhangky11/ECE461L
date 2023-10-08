617221cc5b76d87ee031dd989b7d2c409b09acfa
Kevin Tong <ktong314@gmail.com>
2023-10-08 15:51:31 -0500

    added userContext to save username after login

--- frontend/src/Components/UserProfile/UserProfilePage.jsx
+++ frontend/src/Components/UserProfile/UserProfilePage.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, {useState, useContext} from 'react'
+import { UserContext } from '../../App'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-const UserProfilePage = ({username, Logout}) => {
-
+const UserProfilePage = () => {
+  const navigate = useNavigate();
+  const [user, setUser] = useContext(UserContext)
+  const LogoutHandler = () => {
+    navigate('/login');
+  }
   return (
     <div>
-        <div>Welcome, {username}</div>
-        <button onClick={Logout}>Logout</button>
+        <div>Welcome, {user}</div>
+        <button onClick={LogoutHandler}>Logout</button>
     </div>
   )
 }
