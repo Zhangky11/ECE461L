08e688e6aaab2a939299a15c5942fdced69b67c0
P07-Vik <vikramp.21014@gmail.com>
2023-11-11 16:56:45 -0600

    Heroku changes made

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,18 @@
+from flask import Flask
 from backend import app
 from backend.auth.model import User
 from backend.api.project.model import Project
 from backend.api.hardware.model import HwSet
 from backend.shared.hardware_pool import HardwarePool
+import os
+
+app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
+
+@app.route('/', methods=["GET"])
+def index():
+    return app.send_static_file('index.html')
+
+app.send_static_file('index.html')
 
 if __name__ == "__main__":
     HardwarePool.objects().delete()
@@ -13,4 +23,4 @@ if __name__ == "__main__":
     User.objects().delete()
     Project.objects().delete()
     HwSet.objects().delete()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 80))
