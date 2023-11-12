d673161458af0ccb8092e2f646bc56943b29537b
P07-Vik <vikramp.21014@gmail.com>
2023-11-11 18:12:24 -0600

    attempt revert

--- run.py
+++ run.py
@@ -1,17 +1,8 @@
-from xmlrpc.client import TRANSPORT_ERROR
-from flask import Flask
 from backend import app
 from backend.auth.model import User
 from backend.api.project.model import Project
 from backend.api.hardware.model import HwSet
 from backend.shared.hardware_pool import HardwarePool
-import os
-
-app = Flask(__name__, static_folder='frontend/build', static_url_path='/')
-
-@app.route('/', methods=["GET"])
-def index():
-    return app.send_static_file('index.html')
 
 if __name__ == "__main__":
     HardwarePool.objects().delete()
@@ -23,4 +14,3 @@ if __name__ == "__main__":
     Project.objects().delete()
     HwSet.objects().delete()
     app.run(debug=True)
-    #app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 80))
