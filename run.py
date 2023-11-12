1b1a7ee025eac30bc27be4da100a4a18108450b1
P07-Vik <vikramp.21014@gmail.com>
2023-11-11 23:59:12 -0600

    code

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
+import flask
 from backend import app
 from backend.auth.model import User
 from backend.api.project.model import Project
 from backend.api.hardware.model import HwSet
 from backend.shared.hardware_pool import HardwarePool
+import os
+
+app = flask(__name__, static_folder='./frontend/build', static_url_path='/')
+
+@app.route('/', methods=["GET"])
+def index():
+    return app.send_static_file('index.html')
 
 if __name__ == "__main__":
     HardwarePool.objects().delete()
@@ -13,4 +21,5 @@ if __name__ == "__main__":
     User.objects().delete()
     Project.objects().delete()
     HwSet.objects().delete()
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
