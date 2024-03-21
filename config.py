cea5f1cfa25df3a7f95a35165d339d62c94a2f76
Jiaju Wang <96708489+jiajuwang@users.noreply.github.com>
2024-03-20 19:52:31 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,7 @@ import os
 class Config:
     SECRET_KEY = 'your_fallback_secret_key'
 
-    # Use your own MongoDB host
-    # MONGODB_SETTINGS = {
-    #        'db': 'EE461LProject',
-    #       'host': 'mongodb+srv://jiajuwang:WJj25127358@cluster0.q7f7ign.mongodb.net/'
-    #   }
+
     MONGODB_SETTINGS = {
     'db': 'EE461LProject',
     'host': "mongodb+srv://zhangky03:CMH4lvBpn7CnC5SX@cluster0.rx7pay3.mongodb.net/"
